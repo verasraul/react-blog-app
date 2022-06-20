7f8d716acd5c2b6422d9bdc937341326178d1b34
Raul Veras <verasraul@gmail.com>
2022-06-20 00:13:42 -0400

    updating HomePage component

--- client/src/components/shared/HomePage.jsx
+++ client/src/components/shared/HomePage.jsx
@@ -5,15 +5,52 @@ import {
     setSignedIn,
     setUserData,
 } from "../../features/userSlice";
+
 import "../../styling/home.css";
 
 
 function HomePage() {
+    const isSignedIn = useSelector(selectSignedIn);
+
+    const dispatch = useDispatch();
+    const login = (response) => {
+        console.log(response);
+        dispatch(setSignedIn(true));
+        dispatch(setUserData(response.profilesObj));
+    };
+
+
   return (
-    <div>
-       Welcome to HomePage
+    <div className="home-page" style={{ display: isSignedIn ? "none" : "" }}>
+       {!isSignedIn ? (
+        <div className="login-message">
+            <h2>_</h2>
+            <h1> A Readers Favorite Place!</h1>
+            <p>
+                We provide high quality online resource for reading blogs. Just sign up and start reading some quality blogs.
+            </p>
+            <GoogleLogin
+            clientId="927768120339-qcnrjmt9k77hh0ockktg6cfrktcluelk.apps.googleusercontent.com"
+            render={(renderProps) => (
+                <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login-button"
+                >
+                    Login with Google
+                </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+            />
+        </div>
+       ) : (
+        ""
+       )}
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
