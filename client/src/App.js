48f4aaa15a7efb9f680bac57e9c65600f0b8ba5e
Raul Veras <verasraul@gmail.com>
2022-06-20 04:21:33 -0400

    importing NavBar in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from './components/shared/HomePage';
+import NavBar from './components/shared/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <HomePage />
     </div>
   );
