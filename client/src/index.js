98e532f118fbfa2b9b6df8e53d7b54a85c02b02f
Raul Veras <verasraul@gmail.com>
2022-06-19 22:59:57 -0400

    commenting index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-import { Provider } from 'react-redux'; 
-import store from "./app/store"; 
+// let the app know we have use the google auth using the custome store & reducers 
+import { Provider } from 'react-redux'; // user the configurations from the provider function in redux
+import store from "./app/store"; // user the configurations inside store.jsx
 
 ReactDOM.render(
   <Provider store={store}>
