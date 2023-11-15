a50e908e630d26f6344cd298b3104ea4d40a40e6
Raul Veras <verasraul@gmail.com>
2023-11-15 16:38:56 -0500

    adding comments

--- client/src/components/shared/NavBar.jsx
+++ client/src/components/shared/NavBar.jsx
@@ -1,6 +1,6 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar } from "@material-ui/core"; // Avatar is a materialUI component.
 import { useState } from "react";
-import { GoogleLogout } from "react-google-login";
+import { GoogleLogout } from "react-google-login"; // Log-out option.
 import { useDispatch, useSelector } from "react-redux";
 import {
     selectSignedIn,
@@ -8,40 +8,42 @@ import {
     setInput,
     setSignedIn,
     setUserData,
-} from "../../features/userSlice";
+} from "../../features/userSlice"; // import what's necessary from the userSlice component into NavBar.
 
-import "../../styling/navbar.css";
+import "../../styling/navbar.css"; // import navbar stylesheet to stye component.
 
 function NavBar() {
-    const [inputValue, setInputValue] = useState("tech");
+    // HOOKS
+    const [inputValue, setInputValue] = useState("tech"); // useState hook only changes component necessary without refreshing entire page.
     const isSignedIn = useSelector(selectSignedIn);
     const userData = useSelector(selectUserData);
 
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(); // using dispatch from react-redux.
 
-    const logout = (response) => {
-        dispatch(setSignedIn(false));
-        dispatch(setUserData(null));
+    const logout = (response) => { // when users click logout, it will dispatch the reducer function from userSlice.
+        dispatch(setSignedIn(false)); // then it will set the sign-in to false.
+        dispatch(setUserData(null)); // and set user data to null (inaccessible).
     };
 
     const handleClick = (e) => {
-        e.preventDefault();
-        dispatch(setInput(inputValue));
+        e.preventDefault(); // prevents the entire site from refreshing when clicking search button.
+        dispatch(setInput(inputValue)); // set input value to search string value provided by user.
     };
 
   return (
     <div className="navbar">
         <h1 className="navbar-header">BlogSpot 💬</h1>
-        {isSignedIn && (
-            <div className="blog-search">
-                <input
-                className="search"
-                placeholder="Search for a blog"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
+        {isSignedIn && ( // if the user is signed (isSignedIn = true) only this block of code will be executed.
+            <div className="blog-search"> {/*search-bar division for blog.*/}
+                <input // the search division takes input.
+                className="search" // CSS classname.
+                placeholder="Search for a blog" // this is a placeholder text for input section.
+                value={inputValue} // sets initial input value, set to 'tech'
+                onChange={(e) => setInputValue(e.target.value)} // updates/changes input value for useState hook (setInputValue) from user input. e.target refers to the input element that triggered the event and str.target.value contains the current value of the input.
                 />
-                <button className="submite" onClick={handleClick}>
-                    Search
+                {/*Search button*/}
+                <button className="submit" onClick={handleClick}> {/*when user clicks on search that wil call the 'handleClick function*/}
+                    Search 
                 </button>
             </div>
         )}
@@ -50,8 +52,10 @@ function NavBar() {
             <div className="=navbar-user-data">
                 <Avatar
                     className="user"
-                    src={userData?.imageUrl}
-                    alt={userData?.name}
+                    // userData contains the information of the google user signed in.
+                    src={userData?.imageUrl} // this sources profile picture from the google account signed in.
+                    alt={userData?.name} // alternatively if there's no user pic, username will be shown only.
+                    // userData values come from the userSlice component.
                 />
                 <h1 className="signedIn">{userData?.givenName}</h1>
                 <GoogleLogout
@@ -69,7 +73,7 @@ function NavBar() {
                 />
             </div>
             ) : (
-                <h1 className="notSignedIn">User not avaialble 😞</h1>
+                <h1 className="notSignedIn">User not available 😞</h1>
         )}
     </div>
   )
--- client/src/features/userSlice.jsx
+++ client/src/features/userSlice.jsx
@@ -1,24 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// creating user slice.
 const userSlice = createSlice({
+    // userSlice parameters.
         name: "user",
-        initialState: {
-            isSignedIn: false,
-            userData: null,
-            searchInput: "Tech",
-            blogData: null,
+        initialState: { // initialState is an object with key-pair values.
+            isSignedIn: false, // initial state of user, initially user is not signed in.
+            userData: null, // user is not initially signed-in, therefore userData = null (unavailable).
+            searchInput: "tech", // default search results to render when user is not signed in, initially 'tech' news resuts will be shown.
+            blogData: null, // search results will only render when user is signed in, blogData is initially null (unavaible).
         },
-    reducers: {
-        setSignedIn: (state, action) => {
+    // REDUCERS
+    // reducers are used to set and get data.
+    reducers: { // these reducers are used manipulate the initialState properties.
+        setSignedIn: (state, action) => { // set isSignedIn state.
             state.isSignedIn = action.payload;
         },
-        setUserData: (state, action) => {
+        setUserData: (state, action) => { // set userData state.
             state.userData = action.payload;
         },
-        setInput: (state, action) => {
+        setInput: (state, action) => { // set setInput state.
             state.searchInput = action.paylod;
         },
-        setBlogData: (state, action) => {
+        setBlogData: (state, action) => { // set setInput state.
             state.blogData = action.payload;
         },
     },
